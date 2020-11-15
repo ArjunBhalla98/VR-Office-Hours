bbf4a88c0095cad40accf581926351b7d60b47cc
Xin Chen <xc459@cornell.edu>
2020-11-15 12:58:32 +0800

    Physics Raycaster basic
    - Raycasting from right controller

---
+++ Final-Project-CS5650/Assets/Scripts/Pointer/PhysicsPointer.cs
@@ -0,0 +1,54 @@
+﻿// Attach to oculus controller
+
+using UnityEngine;
+
+public class PhysicsPointer : MonoBehaviour
+{
+    public float defaultLength = 3.0f;
+
+    private LineRenderer lineRenderer = null;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLength();
+    }
+
+    private void UpdateLength()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, CalculateEnd());
+    }
+
+    private Vector3 CalculateEnd()
+    {
+        RaycastHit hit = CreateForwardRaycast();
+        Vector3 endPosition = DefaultEnd(defaultLength);
+
+        if (hit.collider)
+            endPosition = hit.point;
+
+        return endPosition;
+    }
+
+    private RaycastHit CreateForwardRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        Physics.Raycast(ray, out hit, defaultLength);
+        return hit;
+    }
+
+    // In case nothing is hit
+    private Vector3 DefaultEnd(float length)
+    {
+        return transform.position + (transform.forward * length);
+    }
+
+
+}
