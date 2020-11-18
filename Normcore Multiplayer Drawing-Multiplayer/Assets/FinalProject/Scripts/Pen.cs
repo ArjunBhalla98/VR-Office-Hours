9f12b3a04332c1cf01c05e32d31af433901b084e
Xin Chen <xxiinnchen@gmail.com>
2020-11-18 16:43:00 +0800

    Implement texture mapping, buggy
    - Mesh Collidor not working

--- Normcore Multiplayer Drawing-Multiplayer/Assets/FinalProject/Scripts/Pen.cs
+++ Normcore Multiplayer Drawing-Multiplayer/Assets/FinalProject/Scripts/Pen.cs
@@ -29,13 +29,14 @@ public class Pen : MonoBehaviour
     private Quaternion _handRotation;
     private BrushStroke _activeBrushStroke;
 
-    /// <summary>
-    /// Pen raycast variables for detecting if the PenTip touches the whiteboard
-    /// </summary>
+    // board writing on
     public Whiteboard _whiteboard;
     private RaycastHit _touch;
     private bool _isTouching; // Is the pentip is in contact with the whiteboard
     float tipHeight = 0.3f; // TODO: fine tune the distance to make writing smoother
+
+    // color of the brush
+    public Color32 _penColor;
     ///////////////// End Writing ////////////////
 
     // LineRenderer lr;
@@ -79,9 +80,13 @@ public class Pen : MonoBehaviour
             // touches the Whiteboard
             OVRInput.SetControllerVibration(1f, 0.1f, OVRInput.Controller.RTouch);
 
-            this._whiteboard.SetColor(Color.blue);
-            this._whiteboard.SetTouchPosition(_touch.textureCoord.x, _touch.textureCoord.y);
-            this._whiteboard.ToggleTouch(true);
+            //this._whiteboard.SetColor(Color.blue);
+            //this._whiteboard.SetTouchPosition(_touch.textureCoord.x, _touch.textureCoord.y);
+            //this._whiteboard.ToggleTouch(true);
+            _whiteboard.SetTouchPositon(_touch.textureCoord.x, _touch.textureCoord.y);
+            //Current pen color
+            _whiteboard.SetColor(new Color32(0, 255, 0, 255));
+            _whiteboard.IsDrawing = true;
         } else
         {
             _isTouching = false;
@@ -104,82 +109,82 @@ public class Pen : MonoBehaviour
     /// Modification of Normcore drawing by enabling drawing from the pentip
     /// using BrushStroke
     /// </summary>
-    void UpdateDrawBrush()
-    {
-        if (!_realtime.connected)
-            return;
+//    void UpdateDrawBrush()
+//    {
+//        if (!_realtime.connected)
+//            return;
 
-        // Start by figuring out which hand we're tracking
-        XRNode node = _hand == Hand.LeftHand ? XRNode.LeftHand : XRNode.RightHand;
-        string trigger = _hand == Hand.LeftHand ? "Left Trigger" : "Right Trigger";
+//        // Start by figuring out which hand we're tracking
+//        XRNode node = _hand == Hand.LeftHand ? XRNode.LeftHand : XRNode.RightHand;
+//        string trigger = _hand == Hand.LeftHand ? "Left Trigger" : "Right Trigger";
 
-        // Get the position & rotation of the hand
-        bool handIsTracking = UpdatePose(node, ref _handPosition, ref _handRotation);
+//        // Get the position & rotation of the hand
+//        bool handIsTracking = UpdatePose(node, ref _handPosition, ref _handRotation);
 
-        // Figure out if the trigger is pressed or not
-        bool triggerPressed = Input.GetAxisRaw(trigger) > 0.1f;
+//        // Figure out if the trigger is pressed or not
+//        bool triggerPressed = Input.GetAxisRaw(trigger) > 0.1f;
 
-        // If we lose tracking, stop drawing
-        if (!handIsTracking)
-            triggerPressed = false;
+//        // If we lose tracking, stop drawing
+//        if (!handIsTracking)
+//            triggerPressed = false;
 
-        // If the trigger is pressed and PenTip touches the board,
-        // and we haven't created a new brush stroke to draw, create one!
-        if (triggerPressed && _isTouching && _activeBrushStroke == null)
-        {
-            // Instantiate a copy of the Brush Stroke prefab, set it to be owned by us.
-            GameObject brushStrokeGameObject = Realtime.Instantiate(_brushStrokePrefab.name, ownedByClient: true, useInstance: _realtime);
+//        // If the trigger is pressed and PenTip touches the board,
+//        // and we haven't created a new brush stroke to draw, create one!
+//        if (triggerPressed && _isTouching && _activeBrushStroke == null)
+//        {
+//            // Instantiate a copy of the Brush Stroke prefab, set it to be owned by us.
+//            GameObject brushStrokeGameObject = Realtime.Instantiate(_brushStrokePrefab.name, ownedByClient: true, useInstance: _realtime);
 
-            // Grab the BrushStroke component from it
-            _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke>();
+//            // Grab the BrushStroke component from it
+//            _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke>();
 
-            // Tell the BrushStroke to begin drawing at the current brush position
-            //_activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
-            _activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
-        }
+//            // Tell the BrushStroke to begin drawing at the current brush position
+//            //_activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
+//            _activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+//        }
 
-        // If the trigger is pressed and PenTip touches the board,
-        // and we have a brush stroke, move the brush stroke to the new brush tip position
-        if (triggerPressed && _isTouching)
-            //_activeBrushStroke.MoveBrushTipToPoint(_handPosition, _handRotation);
-            _activeBrushStroke.MoveBrushTipToPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+//        // If the trigger is pressed and PenTip touches the board,
+//        // and we have a brush stroke, move the brush stroke to the new brush tip position
+//        if (triggerPressed && _isTouching)
+//            //_activeBrushStroke.MoveBrushTipToPoint(_handPosition, _handRotation);
+//            _activeBrushStroke.MoveBrushTipToPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
 
-        // If the trigger is no longer pressed and the PenTip touches the board,
-        // and we still have an active brush stroke, mark it as finished and clear it.
-        if (!triggerPressed && _isTouching && _activeBrushStroke != null)
-        {
-            //_activeBrushStroke.EndBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
-            _activeBrushStroke.EndBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
-            _activeBrushStroke = null;
-        }
-    }
+//        // If the trigger is no longer pressed and the PenTip touches the board,
+//        // and we still have an active brush stroke, mark it as finished and clear it.
+//        if (!triggerPressed && _isTouching && _activeBrushStroke != null)
+//        {
+//            //_activeBrushStroke.EndBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
+//            _activeBrushStroke.EndBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+//            _activeBrushStroke = null;
+//        }
+//    }
 
-    //// Utility
+//    //// Utility
 
-    // Given an XRNode, get the current position & rotation. If it's not tracking, don't modify the position & rotation.
-    private static bool UpdatePose(XRNode node, ref Vector3 position, ref Quaternion rotation)
-    {
-        List<XRNodeState> nodeStates = new List<XRNodeState>();
-        InputTracking.GetNodeStates(nodeStates);
+//    // Given an XRNode, get the current position & rotation. If it's not tracking, don't modify the position & rotation.
+//    private static bool UpdatePose(XRNode node, ref Vector3 position, ref Quaternion rotation)
+//    {
+//        List<XRNodeState> nodeStates = new List<XRNodeState>();
+//        InputTracking.GetNodeStates(nodeStates);
 
-        foreach (XRNodeState nodeState in nodeStates)
-        {
-            if (nodeState.nodeType == node)
-            {
-                Vector3 nodePosition;
-                Quaternion nodeRotation;
-                bool gotPosition = nodeState.TryGetPosition(out nodePosition);
-                bool gotRotation = nodeState.TryGetRotation(out nodeRotation);
+//        foreach (XRNodeState nodeState in nodeStates)
+//        {
+//            if (nodeState.nodeType == node)
+//            {
+//                Vector3 nodePosition;
+//                Quaternion nodeRotation;
+//                bool gotPosition = nodeState.TryGetPosition(out nodePosition);
+//                bool gotRotation = nodeState.TryGetRotation(out nodeRotation);
 
-                if (gotPosition)
-                    position = nodePosition;
-                if (gotRotation)
-                    rotation = nodeRotation;
+//                if (gotPosition)
+//                    position = nodePosition;
+//                if (gotRotation)
+//                    rotation = nodeRotation;
 
-                return gotPosition;
-            }
-        }
+//                return gotPosition;
+//            }
+//        }
 
-        return false;
-    }
+//        return false;
+//    }
 }
--- Normcore Multiplayer Drawing-Multiplayer/Assets/FinalProject/Scripts/Whiteboard.cs
+++ Normcore Multiplayer Drawing-Multiplayer/Assets/FinalProject/Scripts/Whiteboard.cs
@@ -11,34 +11,76 @@ public class Whiteboard : MonoBehaviour
     OVRGrabbable m_GrabState;
     bool isGrabbed = false;
 
-    /// <summary>
-    /// Using texture mapping on the whiteboard
-    /// </summary>
-    private int _textureSize = 2048;
-    private int _penSize = 10;
-    private Texture2D _texture;
-    private Color[] _color;
+    //When the brush moves very fast, in order to avoid intermittent points,
+    // it is necessary to interpolate between two points,
+    // and LERP is the interpolation coefficient
+    [Range(0, 1)]
+    public float lerp = 0.05f;
 
-    private bool _touching, _touchingLast;
-    private float _posX, _posY;
-    private float _lastX, lastY;
+    // Texture for writing on the board
+    private Texture2D _texture;
+    //The position of the brush is mapped to the UV coordinates of the board texture
+    private Vector2 _paintPos;
+    private bool _isTouching = false;
+
+    //Where the brush is when you leave 
+    private int _lastX, _lastY;
+
+    //The size of the color block represented by the brush
+    private int _painterTipsWidth = 30;
+    private int _painterTipsHeight = 15;
+
+    //The size of the background picture of the current palette
+    private int _textureWidth;
+    private int _textureHeight;
+
+    //The color of the brush
+    private Color32[] _color;
+
+
+    //private int _textureSize = 2048;
+    //private int _penSize = 10;
+    ////private bool _touching, _touchingLast;
+    //private int _posX, _posY;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Grabbing
         rb = GetComponent<Rigidbody>();
         m_GrabState = GetComponent<OVRGrabbable>();
         m_GrabOffset = GameObject.Find("WhiteboardGrabOffset").transform;
         m_GrabState.snapOffset = m_GrabOffset;
 
-        Renderer renderer = GetComponent<Renderer>();
-        this._texture = new Texture2D(_textureSize, _textureSize);
-        renderer.material.mainTexture = this._texture;
+        //Renderer renderer = GetComponent<Renderer>();
+        //this._texture = new Texture2D(_textureSize, _textureSize);
+        //renderer.material.mainTexture = this._texture;
+
+        //this.SetColor(Color.blue);
+        //_texture.SetPixels(2000, 2000, _penSize, _penSize, _color);
+        //_texture.Apply();
+
+        //Get the size of the original texture of the board
+        Texture2D originTexture = GetComponent<MeshRenderer>().material.mainTexture as Texture2D;
+        _textureWidth = originTexture.width; //1920 
+        _textureHeight = originTexture.height; //1080
+
+        //Set current picture
+        _texture = new Texture2D(_textureWidth, _textureHeight, TextureFormat.RGBA32, false, true);
+        _texture.SetPixels32(originTexture.GetPixels32());
+        _texture.Apply();
+
+        //Assign to whiteboard
+        GetComponent<MeshRenderer>().material.mainTexture = _texture;
+
+        //Initialize brush color
+        _color = Enumerable.Repeat<Color32>(new Color32(255, 0, 0, 255), _painterTipsWidth * _painterTipsHeight).ToArray<Color32>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Grabbing
         if (m_GrabState.isGrabbed && !isGrabbed)
         {
             isGrabbed = true;
@@ -50,11 +92,57 @@ public class Whiteboard : MonoBehaviour
             rb.constraints = RigidbodyConstraints.FreezeAll; 
 	    }
 
+        //int x = (int)(_posX * _textureSize - (_penSize / 2));
+        //int y = (int)(_posY * _textureSize - (_penSize / 2));
+
+        //if (_touchingLast)
+        //{
+        //    _texture.SetPixels32(x, y, _penSize, _penSize, _color);
+        //    _texture.Apply();
+        //}
+
+        //this._lastX = (float)x;
+        //this._lastY = (float)y;
+
+        //this._touchingLast = this._touching;
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
+    {
+        //Calculate the starting point of the color block represented by the current brush
+        int texPosX = (int)(_paintPos.x * (float)_textureWidth - (float)(_painterTipsWidth / 2));
+        int texPosY = (int)(_paintPos.y * (float)_textureHeight - (float)(_painterTipsHeight / 2));
+        if (_isTouching)
+        {
+            //Change the pixel value of the block where the brush is located
+            _texture.SetPixels32(texPosX, texPosY, _painterTipsWidth, _painterTipsHeight, _color);
+
+            //If you move the brush quickly, there will be intermittent phenomenon, so interpolation is needed
+            if (_lastX != 0 && _lastY != 0)
+            {
+                int lerpCount = (int)(1 / lerp);
+                for (int i = 0; i <= lerpCount; i++)
+                {
+                    int x = (int)Mathf.Lerp((float)_lastX, (float)texPosX, lerp);
+                    int y = (int)Mathf.Lerp((float)_lastY, (float)texPosY, lerp);
+                    _texture.SetPixels32(x, y, _painterTipsWidth, _painterTipsHeight, _color);
+                }
+            }
+            _texture.Apply();
+            _lastX = texPosX;
+            _lastY = texPosY;
+        }
+        else
+        {
+            _lastX = _lastY = 0;
+        }
+    }
+
+
+        private void FixedUpdate()
     {
     }
+
 
     void OnCollisionEnter(Collision other)
     {
@@ -65,17 +153,72 @@ public class Whiteboard : MonoBehaviour
 
     public void ToggleTouch(bool touching)
     {
-        this._touching = touching;
+        this._isTouching = touching;
     }
 
-    public void SetTouchPosition(float x, float y)
+    //public void SetColor(Color color)
+    //{
+    //    this._color = Enumerable.Repeat<Color>(color, _penSize * _penSize).ToArray<Color>();
+    //}
+
+    /// <summary>
+    ///Set the UV position of the current brush
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    public void SetTouchPositon(float x, float y)
     {
-        this._posX = x;
-        this._posY = y;
+        _paintPos.Set(x, y);
     }
 
-    public void SetColor(Color color)
+    /// <summary>
+    ///Is the brush drawing at present
+    /// </summary>
+    public bool IsDrawing
     {
-        this._color = Enumerable.Repeat<Color>(color, _penSize * _penSize).ToArray<Color>();
+        get
+        {
+            return _isTouching;
+        }
+        set
+        {
+            _isTouching = value;
+        }
+    }
+
+    /// <summary>
+    ///Use the color of the brush currently on the palette
+    /// </summary>
+    /// <param name="color"></param>
+    public void SetColor(Color32 color)
+    {
+        if (!_color[0].IsEqual(color))
+        {
+            for (int i = 0; i < _color.Length; i++)
+            {
+                _color[i] = color;
+            }
+        }
+    }
+}
+
+public static class MethodExtention
+{
+    /// <summary>
+    ///Used to compare two color32 types for the same color
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="compare"></param>
+    /// <returns></returns>
+    public static bool IsEqual(this Color32 origin, Color32 compare)
+    {
+        if (origin.g == compare.g && origin.r == compare.r)
+        {
+            if (origin.a == compare.a && origin.b == compare.b)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
