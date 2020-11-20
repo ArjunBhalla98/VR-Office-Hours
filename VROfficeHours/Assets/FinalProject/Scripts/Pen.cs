38ac1b4bb5ce5bee2e083604542e16065e9ca1a8
ArjunBhalla98 <arjunbhallasg@gmail.com>
2020-11-20 18:23:23 +0800

    Pen-board stop, masking on wb, terrain updates, bugfixes, QoL improvements, Full project hierarchy redo

---
+++ VROfficeHours/Assets/FinalProject/Scripts/Pen.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Pen : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_PenTip;
+
+    /// <summary>
+    /// Using Normcore brush mesh
+    /// </summary>
+    // Reference to Realtime to use to instantiate brush strokes
+    [SerializeField] private Realtime _realtime = null;
+
+    // Prefab to instantiate when we draw a new brush stroke
+    [SerializeField] private GameObject _brushStrokePrefab = null;
+
+    // Which hand should this brush instance track?
+    private enum Hand { LeftHand, RightHand };
+    [SerializeField] private Hand _hand = Hand.RightHand;
+
+    // Used to keep track of the current brush tip position and the actively drawing brush stroke
+    private Vector3 _handPosition;
+    private Quaternion _handRotation;
+    //private BrushStroke _activeBrushStroke;
+
+    /// <summary>
+    /// Texture mapping
+    /// </summary>
+    // board writing on
+    public Whiteboard _whiteboard;
+    private RaycastHit _touch;
+    RaycastHit _stillInRange;
+    private bool _isTouching; // Is the pentip is in contact with the whiteboard
+    float tipHeight = 0.1f; // TODO: fine tune the distance to make writing smoother
+
+    // color of the brush
+    public Color32 _penColor;
+
+    // For making sure the pen doesn't go through the board
+    float lastZPosition;
+    //LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lr = GetComponent<LineRenderer>();
+
+        this._whiteboard = GameObject.Find("Whiteboard").GetComponent<Whiteboard>();
+
+        _isTouching = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lr.SetPosition(0, transform.position);
+        //lr.SetPosition(1, transform.position + transform.forward);
+
+        //   if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward))
+        //   { 
+
+        //}
+        //float tipHeight = m_PenTip.transform.localScale.y;
+
+        Vector3 tip = m_PenTip.transform.position;
+        //Debug.DrawRay(tip, transform.forward, Color.green);
+
+        // Start by figuring out which hand we're tracking
+        XRNode node = _hand == Hand.LeftHand ? XRNode.LeftHand : XRNode.RightHand;
+        string trigger = _hand == Hand.LeftHand ? "Left Trigger" : "Right Trigger";
+
+        // Figure out if the trigger is pressed or not
+        //bool triggerPressed = Input.GetAxisRaw(trigger) > 0.1f;
+
+        if (Physics.Raycast(transform.position, transform.forward, out _touch, tipHeight))
+        {
+            if (!(_touch.collider.tag.Equals("Whiteboard")))
+                return;
+
+            if (!_isTouching)
+            {
+                lastZPosition = transform.position.z;
+	        }
+
+			transform.position = new Vector3(transform.position.x, transform.position.y, lastZPosition);
+
+            this._whiteboard = _touch.collider.GetComponent<Whiteboard>();
+            RealtimeTransform whiteboardTransform = _whiteboard.GetComponent<RealtimeTransform>();
+            whiteboardTransform.RequestOwnership();
+
+            _isTouching = true;
+
+            // Haptic feedback for writing, vibrates when the controller
+            // touches the Whiteboard
+            OVRInput.SetControllerVibration(1f, 0.1f, OVRInput.Controller.RTouch);
+
+			_whiteboard.SetTouchPositon(_touch.textureCoord.x, _touch.textureCoord.y);
+			//Current pen color
+			_whiteboard.SetColor(new Color32(0, 255, 0, 255));
+			_whiteboard.IsDrawing = true;
+        }
+        else
+        {
+            //if (Physics.Raycast(transform.position - 0.1f * transform.forward, transform.forward, out _stillInRange, 0.1f) && _isTouching)
+            //{
+            //    transform.position = new Vector3(transform.position.x, transform.position.y, lastZPosition);
+            //}
+            //else
+            //{ 
+		    _isTouching = false;
+		    // Haptic feedback for writing, stop vibration when controller
+		    // leaves the Whiteboard
+		    OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+	    
+	        //}
+
+        }
+
+        //UpdateDrawBrush(); // Using normcore ribben-like brush texture
+    }
+
+    /// <summary>
+    /// Modification of Normcore drawing by enabling drawing from the pentip
+    /// using BrushStroke
+    /// </summary>
+    //    void UpdateDrawBrush()
+    //    {
+    //        if (!_realtime.connected)
+    //            return;
+
+    //        // Start by figuring out which hand we're tracking
+    //        XRNode node = _hand == Hand.LeftHand ? XRNode.LeftHand : XRNode.RightHand;
+    //        string trigger = _hand == Hand.LeftHand ? "Left Trigger" : "Right Trigger";
+
+    //        // Get the position & rotation of the hand
+    //        bool handIsTracking = UpdatePose(node, ref _handPosition, ref _handRotation);
+
+    //        // Figure out if the trigger is pressed or not
+    //        bool triggerPressed = Input.GetAxisRaw(trigger) > 0.1f;
+
+    //        // If we lose tracking, stop drawing
+    //        if (!handIsTracking)
+    //            triggerPressed = false;
+
+    //        // If the trigger is pressed and PenTip touches the board,
+    //        // and we haven't created a new brush stroke to draw, create one!
+    //        if (triggerPressed && _isTouching && _activeBrushStroke == null)
+    //        {
+    //            // Instantiate a copy of the Brush Stroke prefab, set it to be owned by us.
+    //            GameObject brushStrokeGameObject = Realtime.Instantiate(_brushStrokePrefab.name, ownedByClient: true, useInstance: _realtime);
+
+    //            // Grab the BrushStroke component from it
+    //            _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke>();
+
+    //            // Tell the BrushStroke to begin drawing at the current brush position
+    //            //_activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
+    //            _activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+    //        }
+
+    //        // If the trigger is pressed and PenTip touches the board,
+    //        // and we have a brush stroke, move the brush stroke to the new brush tip position
+    //        if (triggerPressed && _isTouching)
+    //            //_activeBrushStroke.MoveBrushTipToPoint(_handPosition, _handRotation);
+    //            _activeBrushStroke.MoveBrushTipToPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+
+    //        // If the trigger is no longer pressed and the PenTip touches the board,
+    //        // and we still have an active brush stroke, mark it as finished and clear it.
+    //        if (!triggerPressed && _isTouching && _activeBrushStroke != null)
+    //        {
+    //            //_activeBrushStroke.EndBrushStrokeWithBrushTipPoint(_handPosition, _handRotation);
+    //            _activeBrushStroke.EndBrushStrokeWithBrushTipPoint(m_PenTip.transform.position, m_PenTip.transform.rotation);
+    //            _activeBrushStroke = null;
+    //        }
+    //    }
+
+    //    //// Utility
+
+    //    // Given an XRNode, get the current position & rotation. If it's not tracking, don't modify the position & rotation.
+    //    private static bool UpdatePose(XRNode node, ref Vector3 position, ref Quaternion rotation)
+    //    {
+    //        List<XRNodeState> nodeStates = new List<XRNodeState>();
+    //        InputTracking.GetNodeStates(nodeStates);
+
+    //        foreach (XRNodeState nodeState in nodeStates)
+    //        {
+    //            if (nodeState.nodeType == node)
+    //            {
+    //                Vector3 nodePosition;
+    //                Quaternion nodeRotation;
+    //                bool gotPosition = nodeState.TryGetPosition(out nodePosition);
+    //                bool gotRotation = nodeState.TryGetRotation(out nodeRotation);
+
+    //                if (gotPosition)
+    //                    position = nodePosition;
+    //                if (gotRotation)
+    //                    rotation = nodeRotation;
+
+    //                return gotPosition;
+    //            }
+    //        }
+
+    //        return false;
+    //    }
+}
---
+++ VROfficeHours/Assets/FinalProject/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    Whiteboard m_WhiteboardPrefab;
+
+    [SerializeField]
+    Transform m_PlayerBase;
+
+    public float m_WhiteboardSpawnOffset;
+    
+    bool isSpawned = false;
+    const float k_respawnTime = 1f;
+    float respawnTimer;
+
+    const string m_PrefabName = "Whiteboard";
+
+    Realtime _realtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnTimer = k_respawnTime;
+        _realtime = GetComponent<Realtime>();
+        _realtime.didConnectToRoom += _realtime_didConnectToRoom;
+    }
+
+    private void _realtime_didConnectToRoom(Realtime realtime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.One) && !isSpawned)
+        {
+            GameObject instantiatedWhiteboard = Realtime.Instantiate(m_PrefabName,
+                position: m_PlayerBase.position + m_WhiteboardSpawnOffset * m_PlayerBase.forward,
+                rotation: Quaternion.identity,
+                ownedByClient: false,
+                preventOwnershipTakeover: false,
+                useInstance: _realtime);
+
+            instantiatedWhiteboard.transform.LookAt(m_PlayerBase);
+            isSpawned = true;
+        }
+        else if (isSpawned)
+        {
+            respawnTimer -= Time.deltaTime;
+            if (respawnTimer <= 0)
+            {
+                isSpawned = false;
+                respawnTimer = k_respawnTime; 
+	        }
+	    }
+    }
+}
